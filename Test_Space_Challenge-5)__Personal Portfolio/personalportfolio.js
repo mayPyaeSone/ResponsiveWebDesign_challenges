60a8748fff2da2146b8cdc8425e11248b45ea3f4
May Pyae Sone Nyein Win <nyeinwin04@gmail.com>
2023-11-17 14:25:51 +0630

    first commit

---
+++ Test_Space_Challenge-5)__Personal Portfolio/personalportfolio.js
@@ -0,0 +1,6 @@
+console.log("Width: " + screen.width + ": Height: " + screen.height);
+
+function myFunction() {
+    document.getElementById("welcome-section").style.height = "50px";
+    // document.getElementById("myBtn").style.height =;
+}
---
+++ Test_Space_Challenge-5)__Personal Portfolio/test_)hightfromvar.js
@@ -0,0 +1,7 @@
+console.log("Width: " + screen.width + ": Height: " + screen.height);
+console.log("Inner Width: " + window.innerWidth + ": Outter width: " + window.outerWidth);
+
+function myFunction() {
+    document.getElementById("container").style.height = "100px";
+    // document.getElementById("myBtn").style.height =;
+}
